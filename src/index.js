205b2dc38c9cc0a2615405a74bb01a39a2114fec
@相玉 <xiangyu@MacBook-Pro.local>
2020-02-15 15:30:09 +0800

    [IMP]Simple redex realized

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-// createContext是react提供的一个用于跨组件传值的方法
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { CounterProvider } from './counterStore'
-import App from './App'
-// createContext这个方法的结果是一个对象,里面有两个组件Provider和Consumer
-// Provider用与提供数据
-// Consumer用于接收(消费)数据
 
 
-ReactDOM.render(
-<CounterProvider>
-  {/* {组件及后代组件数据透视共享提供者的value数据} */}
-   <App />
-</CounterProvider>, document.getElementById('root') )
+
+ReactDOM.render(<div>HOC</div>, document.getElementById('root'))
